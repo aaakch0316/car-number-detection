566a27628dedce4502b4d7e14f0b4252f15f64b4
hanpy <hanpy@deepbrainai.io>
2022-11-27 16:39:25 +0900

    file cleaning

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          한시경 프로젝트 첫 페이지.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
