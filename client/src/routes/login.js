23bf905b0dede5f3bb4a56a014c5c4ff50e2cfa5
tidyp <nonillion1030@gmail.com>
2022-12-04 21:24:39 +0900

    Auth [+] login and signup init settings

--- client/src/routes/login.js
+++ client/src/routes/login.js
@@ -1,7 +1,52 @@
-export default function Login() {
+import React, { useState } from 'react';
+import styles from './login.module.scss';
+// import "./login.css"
+
+const Login = () => {
+  const [enteredId, setEnteredId] = useState("")
+  const [enteredPw, setEnteredPw] = useState("")
+
+  const idChangeHandler = (event) => {
+    setEnteredId(event.target.value);
+  };
+
+  const pwChangeHandler = (event) => {
+    setEnteredPw(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const ExpenseData = {
+      id: enteredId,
+      pw: enteredPw,
+    };
+    console.log(ExpenseData)
+    setEnteredId()
+    setEnteredPw()
+  };
+
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Login</h2>
-    </main>
-  );
+    <div className={styles.aaa}>
+      <form onSubmit={submitHandler}>
+        <h2>Login</h2>
+
+        <div className={styles.controls}>
+          <div className={styles.control}>
+            <label>ID</label>
+            <input type="text" value={enteredId} onChange={idChangeHandler} />
+          </div>
+          <div className={styles.control}>
+            <label>PW</label>
+            <input type="text" value={enteredPw} onChange={pwChangeHandler} />
+          </div>
+        </div>
+        <div className={styles.actions}>
+          <button type='submit'>로그인</button>
+        </div>
+      </form>
+    </div >
+  )
 }
+
+export default Login;
--- client/src/routes/signup.js
+++ client/src/routes/signup.js
@@ -1,7 +1,78 @@
-export default function Signup() {
+import React, { useState } from 'react';
+import styles from './signup.module.scss';
+// import "./signup.css"
+
+const Login = () => {
+  const [enteredId, setEnteredId] = useState("")
+  const [enteredPw, setEnteredPw] = useState("")
+  const [enteredPwCheck, setEnteredPwCheck] = useState("")
+  const [enteredName, setEnteredName] = useState("")
+
+  const idChangeHandler = (event) => {
+    setEnteredId(event.target.value);
+  };
+
+  const pwChangeHandler = (event) => {
+    setEnteredPw(event.target.value);
+  };
+
+  const pwcheckChangeHandler = (event) => {
+    setEnteredPwCheck(event.target.value);
+  };
+
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const ExpenseData = {
+      id: enteredId,
+      pw: enteredPw,
+      pwcheck: enteredPwCheck,
+      name: enteredName,
+    };
+    console.log(ExpenseData)
+    setEnteredId("")
+    setEnteredPw("")
+    setEnteredPwCheck("")
+    setEnteredName("")
+
+  };
+
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Signup</h2>
-    </main>
-  );
+    <div className={styles.card}>
+      <form className={styles.aaa}
+        onSubmit={submitHandler}>
+        <h2>sign up</h2>
+
+        <div className={styles.controls}>
+          <div className={styles.control}>
+            <label>아이디</label>
+            <input type="text" value={enteredId} onChange={idChangeHandler} />
+          </div>
+
+          <div className={styles.control}>
+            <label>비밀번호</label>
+            <input type="text" value={enteredPw} onChange={pwChangeHandler} />
+          </div>
+          <div className={styles.control}>
+            <label>비밀번호 확인</label>
+            <input type="text" value={enteredPwCheck} onChange={pwcheckChangeHandler} />
+          </div>
+          <div className={styles.control}>
+            <label>이름</label>
+            <input type="text" value={enteredName} onChange={nameChangeHandler} />
+          </div>
+
+        </div>
+        <div className={styles.actions}>
+          <button type='submit'>회원가입</button>
+        </div>
+      </form>
+    </div>
+  )
 }
+
+export default Login;
