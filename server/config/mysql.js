27eecc32016a18aaa86f55f53d0ffb2183102ecc
hanpy <hanpy@deepbrainai.io>
2022-12-18 16:31:50 +0900

    mysql config settgins

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host : process.env.MYSQL_URL,
-    port : process.env.MYSQL_URL,
-    user : process.env.MYSQL_URL,
-    password : process.env.MYSQL_URL,
+    host : process.env.MYSQL_HOST,
+    port : process.env.MYSQL_PORT,
+    user : process.env.MYSQL_USER,
+    password : process.env.MYSQL_PASSWORD,
     database : process.env.MYSQL_DATABASE
 }
